5ab9e37b869efbd75421e8dd1bdfb12f5d5c36ec
Fayza Nizma Safaya Harda <nglcoair04@gmail.com>
2025-03-27 13:24:19 +0700

    fix: data

--- README.md
+++ README.md
@@ -3,8 +3,8 @@ Fayza Nizma Safaya Harda | fayzanizmash@gmail.com
 
 
 # Additional Link
-Database structure : 
+Database structure :  https://dbdocs.io/fayzanizmash/chatting-system?view=relationships
 
-The extended JSON format, supporting multiple message types (text, images, videos, PDFs) and both single and group chat features : 
+The extended JSON format, supporting multiple message types (text, images, videos, PDFs) and both single and group chat features : https://github.com/nogarlic/chatting/blob/main/public/data/chat.json
 
-Deployment Link : 
+Deployment Link : https://chatting-ruby.vercel.app/
--- src/App.tsx
+++ src/App.tsx
@@ -1,138 +1,100 @@
-import { useEffect, useState } from 'react'
-import Contacts from './components/Contacts'
-import ChatRoom from './components/ChatRoom'
-import { ChatData } from './types'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPhone, faMessage } from '@fortawesome/free-solid-svg-icons'
+import { useEffect, useState } from "react";
+import Contacts from "./components/Contacts";
+import ChatRoom from "./components/ChatRoom";
+import { ChatData } from "./types";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPhone, faMessage } from "@fortawesome/free-solid-svg-icons";
+import { motion } from "framer-motion";
 
 export default function App() {
-    const [chatData, setChatData] = useState<ChatData[] | null>(null)
-    const [selectedRoom, setSelectedRoom] = useState<ChatData | null>(null)
-    const [isMobileChatOpen, setIsMobileChatOpen] = useState(false)
+  const [chatData, setChatData] = useState<ChatData[] | null>(null);
+  const [selectedRoom, setSelectedRoom] = useState<ChatData | null>(null);
+  const [isMobileChatOpen, setIsMobileChatOpen] = useState(false);
 
-    useEffect(() => {
-        fetch('/data/chat.json')
-            .then((res) => res.json())
-            .then((data) => setChatData(data.results))
-            .catch((err) => console.error('Gagal fetch data:', err))
-    }, [])
+  useEffect(() => {
+    fetch("/data/chat.json")
+      .then((res) => res.json())
+      .then((data) => setChatData(data.results))
+      .catch((err) => console.error("Gagal fetch data:", err));
+  }, []);
 
-    return (
-        <div className="flex flex-col xl:flex-row h-screen">
-            <div
-                className={`xl:w-1/4 w-full xl:h-full min-h-screen overflow-x-hidden overflow-y-auto bg-neutral-800 border-r border-gray-900
-        ${isMobileChatOpen ? 'hidden' : 'block'} xl:block`}
-            >
-                {chatData ? (
-                    <Contacts
-                        rooms={chatData}
-                        onSelectRoom={(room) => {
-                            setSelectedRoom(room)
-                            setIsMobileChatOpen(true)
-                        }}
-                        selectedRoom={selectedRoom}
-                    />
-                ) : (
-                    <p className="text-white text-center">Loading...</p>
-                )}
-            </div>
+  return (
+    <div className="flex flex-col xl:flex-row h-screen">
+      <motion.div
+        initial={{ opacity: 0, x: -10 }}
+        animate={{ opacity: 1, x: 0 }}
+        exit={{ opacity: 0, x: -10 }}
+        transition={{ duration: 0.3 }}
+        className={`xl:w-1/4 w-full xl:h-full min-h-screen overflow-x-hidden overflow-y-auto bg-neutral-800 border-r border-gray-900 ${
+          isMobileChatOpen ? "hidden" : "block"
+        } xl:block`}
+      >
+        {chatData ? (
+          <Contacts
+            rooms={chatData}
+            onSelectRoom={(room) => {
+              setSelectedRoom(room);
+              setIsMobileChatOpen(true);
+            }}
+            selectedRoom={selectedRoom}
+          />
+        ) : (
+          <p className="text-white text-center">Loading...</p>
+        )}
+      </motion.div>
 
-            <div
-                className={`w-full xl:w-3/4 h-full overflow-auto ${
-                    isMobileChatOpen ? 'block' : 'hidden'
-                } xl:block`}
-            >
-                {selectedRoom ? (
-                    <>
-                        <ChatRoom
-                            room={selectedRoom.room}
-                            comments={selectedRoom.comments}
-                            onBack={() => setSelectedRoom(null)}
-                        />
-                    </>
-                ) : (
-                    <>
-                        <div>
-                            <div
-                                className={`xl:w-1/4 w-full xl:h-full min-h-screen overflow-x-hidden overflow-y-auto bg-neutral-800 border-r border-gray-900 xl:hidden`}
-                            >
-                                {chatData ? (
-                                    <Contacts
-                                        rooms={chatData}
-                                        onSelectRoom={(room) => {
-                                            setSelectedRoom(room)
-                                            setIsMobileChatOpen(true)
-                                        }}
-                                        selectedRoom={selectedRoom}
-                                    />
-                                ) : (
-                                    <p className="text-white text-center">
-                                        Loading...
-                                    </p>
-                                )}
-                            </div>
-                            <div
-                                className={`flex xl:hidden flex-row w-full px-24 bg-neutral-900 py-3 justify-between absolute bottom-0 rounded-t-2xl`}
-                            >
-                                <div
-                                    className="flex flex-col items-center cursor-pointer"
-                                    onClick={() => setIsMobileChatOpen(false)}
-                                >
-                                    <div className="bg-emerald-900/40 rounded-full p-2">
-                                        <FontAwesomeIcon
-                                            icon={faMessage}
-                                            className="text-white"
-                                        />
-                                    </div>
-                                    <p className="font-bold mt-2 text-white">
-                                        Chats
-                                    </p>
-                                </div>
-                                <div className="flex flex-col items-center">
-                                    <div className="rounded-full p-2">
-                                        <FontAwesomeIcon
-                                            icon={faPhone}
-                                            className="text-white"
-                                        />
-                                    </div>
-                                    <p className="font-bold mt-2 text-white">
-                                        Calls
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                        <p className="text-center xl:block mt-10 hidden">
-                            Pilih chat untuk memulai
-                        </p>
-                    </>
-                )}
-            </div>
+      {selectedRoom ? (
+  <motion.div
+    key={selectedRoom.room.id}
+    initial={{ opacity: 0, x: -10 }} 
+    animate={{ opacity: 1, x: 0 }}
+    exit={{ opacity: 0, x: 10 }}
+    transition={{ duration: 0.3 }}
+    className={`w-full xl:w-3/4 h-full overflow-auto ${
+      isMobileChatOpen ? "block" : "hidden"
+    } xl:block`}
+  >
+    <ChatRoom
+      room={selectedRoom.room}
+      comments={selectedRoom.comments}
+      onBack={() => {
+        setSelectedRoom(null);
+        setIsMobileChatOpen(false);
+      }}
+    />
+  </motion.div>
+) : (
+  <p className="ml-10 xl:block mt-10 hidden">
+    Pilih chat untuk memulai
+  </p>
+)}
 
-            <div
-                className={`${isMobileChatOpen ? 'hidden' : 'flex'} xl:hidden flex-row w-full px-24 bg-neutral-900 py-3 justify-between absolute bottom-0 rounded-t-2xl`}
-            >
-                <div
-                    className="flex flex-col items-center cursor-pointer"
-                    onClick={() => setIsMobileChatOpen(false)}
-                >
-                    <div className="bg-emerald-900/40 rounded-full p-2">
-                        <FontAwesomeIcon
-                            icon={faMessage}
-                            className="text-white"
-                        />
-                    </div>
-                    <p className="font-bold mt-2 text-white">Chats</p>
-                </div>
-                <div className="flex flex-col items-center">
-                    <div className="rounded-full p-2">
-                        <FontAwesomeIcon
-                            icon={faPhone}
-                            className="text-white"
-                        />
-                    </div>
-                    <p className="font-bold mt-2 text-white">Calls</p>
-                </div>
-            </div>
+
+      <motion.div
+        initial={{ y: 20, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        exit={{ y: 20, opacity: 0 }}
+        transition={{ duration: 0.3 }}
+        className={`${
+          isMobileChatOpen ? "hidden" : "flex"
+        } xl:hidden flex-row w-full px-24 bg-neutral-900 py-3 justify-between fixed bottom-0 rounded-t-2xl`}
+      >
+        <div
+          className="flex flex-col items-center cursor-pointer"
+          onClick={() => setIsMobileChatOpen(false)}
+        >
+          <div className="bg-emerald-900/40 rounded-full p-2">
+            <FontAwesomeIcon icon={faMessage} className="text-white" />
+          </div>
+          <p className="font-bold mt-2 text-white">Chats</p>
         </div>
-    )
+        <div className="flex flex-col items-center">
+          <div className="rounded-full p-2">
+            <FontAwesomeIcon icon={faPhone} className="text-white" />
+          </div>
+          <p className="font-bold mt-2 text-white">Calls</p>
+        </div>
+      </motion.div>
+    </div>
+  );
 }
